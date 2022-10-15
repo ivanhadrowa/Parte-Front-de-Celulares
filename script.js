e76cf49f26d3a03883dbf7862138b42b61057150
ivanhadrowa <ivanhadrowa0@gmail.com>
2022-10-15 11:29:16 -0300

    daw

--- script.js
+++ script.js
@@ -12,25 +12,46 @@ function animatedBars(){
 }
 
 
+//PROFE AHI REALICE UN ARRAY Y SE SUMA EL IVA Y TENGO QUE HACER LO MISMO CON LOS DEMAS PRODUCTOS
 
 
-
-
- let products = []
-let total = 0
-
-function add(product, price){
-    console.log(product,price)
-    products.push(product)
-    total = total + price
-    document.getElementById('checkout').innerHTML= `Pagar: $${total}`
-    alert('Producto añadido al carrito')
+function Producto(id, nombre, precio){
+    this.id = id,
+    this.nombre = nombre,
+    this.precio = parseFloat(precio)
 }
 
+
+let iphone = new Producto (1, "iphone", 1300)
+let airpods = new Producto (2, "airpods", 150)
+let cargador = new Producto (3, "cargador", 40)
+let watch = new Producto (4, "watch", 400)
+
+
+let total = 0
+let precioTotal = 0
+
+
+function agregarProducto(){
+    precioTotal = total + iphone.precio
+    let precioTotalConIva = precioTotal * 1.21
+    console.log(precioTotal)
+    document.getElementById('checkout').innerHTML= `Pagar: $${precioTotalConIva}`
+    alert('Producto añadido al carrito se suma el iva')
+    
+}
+
+let agregar = document.getElementById('agg')
+agregar.addEventListener('click', agregarProducto)
+
+
+
+
 function pay(){
-    if(products == 0){
+    let precioTotalConIva = precioTotal * 1.21
+    if(precioTotal == 0){
         alert("No hay productos en el carrito")
     }else{
-    alert("Productos seleccionados: \n" + products.join(", \n") + " \n Total a Pagar: $" + total)
+    alert("Productos seleccionados: \n"  + " \n Total a Pagar con IVA: $" + precioTotalConIva)
 }
 }
